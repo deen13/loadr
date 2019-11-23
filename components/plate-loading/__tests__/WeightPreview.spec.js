a9dc3bfa9574f62338fd4c2f23bba259acdda93c
Dennis Dierkes <dierkes@smartsquare.de>
2019-11-23 23:51:07 +0100

    Display textual weight next to the plate preview and refactor tests

--- components/plate-loading/__tests__/WeightPreview.spec.js
+++ components/plate-loading/__tests__/WeightPreview.spec.js
@@ -1,33 +1,58 @@
-import { mount } from "@vue/test-utils"
-import WeightPreview from "../WeightPreview"
+import { mount } from '@vue/test-utils'
+import WeightPreview from '../WeightPreview'
 
-describe("WeightPreview", () => {
-  it("renders a 70 kg squat correctly", () => {
+describe('WeightPreview', () => {
+  it('renders a 70 kg squat correctly', () => {
+    const onePlate = '<div class="kg25 weight plate plate-kg25"></div>'
+
     const wrapper = mount(WeightPreview, {
       propsData: {
         weight: 70,
         squat: true
-      },
+      }
     })
 
-    expect(wrapper.html()).toContain('<div class="kg25 weight plate plate-kg25"></div>')
+    expect(wrapper.find('#plates-wrapper').html()).toContain(onePlate)
   })
 
-  it("renders a 140kg dip correctly", () => {
+  it('renders a 140kg dip correctly', () => {
+    const allPlates =
+      '<div class="kg25 weight plate plate-kg25"></div>' +
+      '<div class="kg25 weight plate plate-kg25"></div>' +
+      '<div class="kg25 weight plate plate-kg25"></div>' +
+      '<div class="kg25 weight plate plate-kg25"></div>' +
+      '<div class="kg25 weight plate plate-kg25"></div>' +
+      '<div class="kg15 weight plate plate-kg15"></div>'
+
     const wrapper = mount(WeightPreview, {
       propsData: {
         weight: 140,
         squat: false
-      },
+      }
     })
 
-    expect(wrapper.html()).toBe("<div>" +
-      "<div class=\"kg25 weight plate plate-kg25\"></div>" +
-      "<div class=\"kg25 weight plate plate-kg25\"></div>" +
-      "<div class=\"kg25 weight plate plate-kg25\"></div>" +
-      "<div class=\"kg25 weight plate plate-kg25\"></div>" +
-      "<div class=\"kg25 weight plate plate-kg25\"></div>" +
-      "<div class=\"kg15 weight plate plate-kg15\"></div>" +
-      "</div>")
+    expect(wrapper.find('#plates-wrapper').html()).toContain(allPlates)
+  })
+
+  it('renders the textual kg value correctly', () => {
+    const wrapper = mount(WeightPreview, {
+      propsData: {
+        weight: 140,
+        squat: false
+      }
+    })
+
+    expect(wrapper.find('#textual').text()).toEqual("140 Kilogramm")
+  })
+
+  it('renders double numbers in the german format', () => {
+    const wrapper = mount(WeightPreview, {
+      propsData: {
+        weight: 24.5,
+        squat: false
+      }
+    })
+
+    expect(wrapper.find('#textual').text()).toEqual("24,5 Kilogramm")
   })
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <weight-preview :weight="140" squat></weight-preview>
+  <v-row justify="center">
+    <v-col cols="12" class="py-2">
+      <v-row justify="center">
+        <weight-preview :weight="140" squat></weight-preview>
+      </v-row>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
